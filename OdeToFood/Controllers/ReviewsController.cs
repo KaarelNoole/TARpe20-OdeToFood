83115c4939d25ea6b2bcf88283e93594ea10a137
Kaarel Noole <opilane@HP-CZC6297B8M>
2021-12-02 15:32:47 +0200

    Add ReviewerName and views

--- OdeToFood/Controllers/ReviewsController.cs
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -31,5 +31,23 @@ namespace OdeToFood.Controllers
             return View(model);
         }
 
+        [HttpGet]
+        public ActionResult Create(int restaurantId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(RestaurantReview review)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Reviews.Add(review);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index), new { id = review.RestaurantId });
+            }
+
+            return View(review);
+        }
     }
 }
